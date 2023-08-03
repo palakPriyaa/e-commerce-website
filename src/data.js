61b3eb855226013343645018c80f27728e00fbdb
Palak <pp9709735989@gmail.com>
2023-08-03 20:46:13 +0530

    initial commit

---
+++ src/data.js
@@ -0,0 +1,87 @@
+
+//import pic3 from './components/pic3.png'
+
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://i.ibb.co/cXFnLLV/3.png",
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "https://o.remove.bg/downloads/97e6a83c-aaa1-4763-aa65-68d298230971/FEEL_GOOD-removebg-preview.png",
+      title: "AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img:'https://o.remove.bg/downloads/439ea342-c6e5-4094-9330-f5e86a3ed680/download__6_-removebg-preview.png',
+      
+      title: "LOUNGEWEAR LOVE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+  ];
+
+
+
+
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "SHIRT STYLE!",
+    },
+    {
+      id: 2,
+      img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      title: "LOUNGEWEAR LOVE",
+    },
+    {
+      id: 3,
+      img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      title: "LIGHT JACKETS",
+    },
+  ];
+
+
+
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://o.remove.bg/downloads/43effae8-8072-4ce8-ba9a-08192fb69653/Crochet_Panel_Eyelet_Cami_Dress___WHITE-removebg-preview.png",
+    },
+    {
+      id:2,
+      img:"https://o.remove.bg/downloads/7bf158e3-198b-46d2-95e5-df33acb7bcf6/Tie_Back_Butterfly_Sleeve_Blouse-removebg-preview.png",
+    },
+    {
+      id:3,
+      img:"https://o.remove.bg/downloads/3de81f39-e35b-4ae1-b530-731e20e4bbdb/Dresses___Casual__Party_Dresses___More-removebg-preview.png",
+    },
+    {
+      id:4,
+      img:"https://o.remove.bg/downloads/628ab184-bafa-4312-a68a-d043baae0efe/Ready-To-Wear-removebg-preview.png",
+    },
+    {
+      id:5,
+      img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    },
+    {
+      id:6,
+      img:"https://o.remove.bg/downloads/469bdbb7-19e0-449b-bef9-3ff57a8a2984/30_18US____Bohemia_Women_Boho_Blouse_V-neck_Flare_Sleeves_Bow_Kimono_Blouses_Vintage_Floral_Print_Blouses_-_Women_Blouse_-_AliExpress-removebg-preview.png",
+    },
+    {
+      id:7,
+      img:"https://o.remove.bg/downloads/590ef122-dcb7-4b4f-81c7-5368383de155/Dolman_Sleeve_Tie_Open_Back_Surplice_Crop_Top-removebg-preview.png",
+    },
+    {
+      id:8,
+      img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    },
+  ]
